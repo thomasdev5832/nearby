d230605915ee4f2d2815c167b436e5c32e9bc051
thomasdev5832 <eugabrielthome@gmail.com>
2024-12-10 00:21:51 -0300

    init setup + create first components

---
+++ src/components/step/index.tsx
@@ -0,0 +1,22 @@
+import { View, Text } from "react-native"
+import { IconProps } from "@tabler/icons-react-native"
+import { colors } from "@/styles/theme"
+import { s } from "./styles"
+
+type Props = {
+    icon: React.ComponentType<IconProps>
+    title: string
+    description: string
+}
+
+export function Step({ icon: Icon, title, description, }: Props) {
+    return (
+        <View style={s.container}>
+            {Icon && <Icon size={32} color={colors.red.base} />}
+            <View style={s.details}>
+                <Text style={s.title}>{title}</Text>
+                <Text style={s.description}>{description}</Text>
+            </View>
+        </View>
+    )
+}
